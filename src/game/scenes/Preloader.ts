8c13004769fdbee2bd0168e76767acb380a89696
Chap0-0 <vycheslavich@mail.ru>
2024-11-11 02:41:18 +0300

    исправил preloader и изменил структуру

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,47 +1,36 @@
 import { Scene } from 'phaser';
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
+export class Preloader extends Scene {
+    constructor() {
         super('Preloader');
     }
 
-    init ()
-    {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background').setDisplaySize(this.scale.width, this.scale.height).setDepth(0);
+    init() {
+        this.add.image(0, 0, 'background').setOrigin(0, 0).setDisplaySize(this.scale.width, this.scale.height);
 
-        //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        const barWidth = 468;
+        const barHeight = 32;
+        const barX = this.scale.width / 2;
+        const barY = this.scale.height / 2; 
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        this.add.rectangle(barX, barY, barWidth, barHeight).setStrokeStyle(1, 0x602b2c);
 
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+        const bar = this.add.rectangle(barX - barWidth / 2 + 2, barY, 4, barHeight - 4, 0xffffff);
+
         this.load.on('progress', (progress: number) => {
-
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + (460 * progress);
-
+            bar.width = 4 + (barWidth - 8) * progress;
         });
     }
 
-    preload ()
-    {
-        //  Load the assets for the game - Replace with your own assets
-        this.load.setPath('assets');
-        this.load.tilemapTiledJSON('intro-bg-map', 'maps/intro-bg-map.tmj');
-        this.load.image('tiles', 'tiles/Tileset.png');  // Укажите путь к изображению тайлов
+    preload() {
+        // Загрузка активов для игры
+        this.load.setPath('assets/intro');
+        this.load.tilemapTiledJSON('intro-bg-map', 'intro-bg-map.tmj');
+        this.load.image('tiles', 'Tileset.png');
         this.load.image('logo', 'logo.png');
     }
 
-    create ()
-    {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
-
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+    create() {
         this.scene.start('Intro');
     }
 }
